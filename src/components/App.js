9ea1ece745510c069fce5033ec892586433e844c
Suzanne Schuber <suzanneschuber@gmail.com>
2023-09-19 09:08:26 -0700

    Set up Firebase configurations

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <Header>
-      <SignIn></SignIn>
-     </Header>
-    </div>
+
   );
 }
 
